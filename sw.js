f63070f8f2ef77caedb8786a95070de8bbc97c5e
Marcel Spahr <xrp@bluewin.ch>
2026-09-04 16:03:10 +0200

    Compact mobile object and file views

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* Vera Home / Vera Portal – Service Worker
    Bump CACHE_VERSION on future large asset changes so old caches get
    discarded (see activate handler below). */
-const CACHE_VERSION = "vera-pwa-v10";
+const CACHE_VERSION = "vera-pwa-v11";
 const STATIC_CACHE = CACHE_VERSION + "-static";
 const PAGES_CACHE = CACHE_VERSION + "-pages";
 
